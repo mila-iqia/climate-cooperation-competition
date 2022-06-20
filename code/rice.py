f6232b352e8f59c100b4fa255d77e292910be177
Stephan Zheng <stephan.zheng@stephan-ltmh2m4.internal.salesforce.com>
2022-06-20 10:41:53 -0700

    LICENSE and code header draft

--- code/rice.py
+++ code/rice.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021, salesforce.com, inc.
+# Copyright (c) 2022, salesforce.com, inc and MILA.
 # All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 # For full license text, see the LICENSE file in the repo root
