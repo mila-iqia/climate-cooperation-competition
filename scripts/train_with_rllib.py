eb4258f821e8dea4542391b49c292fc6c2975139
pjwozny <phillipjw@gmail.com>
2024-07-31 15:21:01 +0200

    carbonleakagedaamges

--- scripts/train_with_rllib.py
+++ scripts/train_with_rllib.py
@@ -43,7 +43,8 @@ SCENARIO_MAPPING = {
     "BasicClub":BasicClub,
     "ExportAction":ExportAction,
     "CarbonLeakage":CarbonLeakage,
-    "Damages":Damages
+    "Damages":Damages,
+    "CarbonLeakageDamages":CarbonLeakageDamages
 }
 
 def get_config_yaml(yaml_path):
